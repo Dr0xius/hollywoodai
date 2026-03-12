de1891d9acca159ac86a89ab19e5d7892612bc22
Jibrian Goodwin <jjgoodwin346@gmail.com>
2026-03-11 21:51:46 -0400

    fixed deployment error

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,10 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StoreProvider>
-          <AuthSync />
-          {children}
-        </StoreProvider>
+        <StoreProvider>{children}</StoreProvider>
       </body>
     </html>
   );
