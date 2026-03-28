4e9f11b17047b9724bae955058b7e33edabc1cda
Jibrian Goodwin <jjgoodwin346@gmail.com>
2026-03-27 20:05:33 -0400

    changed color on a button to match others for consistency

--- app/settings/components/LoginButton.tsx
+++ app/settings/components/LoginButton.tsx
@@ -9,7 +9,7 @@ const LoginButton = () => {
   return (
     <button
       onClick={() => dispatch(openSignInModal())}
-      className="bg-linear-to-r from-violet-600 via-violet-800 to-violet-900 w-full max-w-50 px-6 py-2 rounded-xl"
+      className="bg-linear-to-r text-xl from-violet-600 via-violet-800 to-violet-900 w-full max-w-50 px-6 py-2 rounded-xl"
     >
       Login
     </button>
